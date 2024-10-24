a55b5071329dc62129afcf575f20145a9c763f5a
Jabarsonrichardgs <jabarsonrichardofficial@gmail.com>
2024-10-25 01:19:46 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import styled from "styled-components";
-import './index.css'
-import  Navbar  from "./components/Navbar";
+import "./index.css";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Features from "./components/Features";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import ContactUs from "./components/ContactUs";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import ExploreCourses from "./components/ExploreCourses";
 const Container = styled.div`
   height: 100vh;
-  
 `;
 
 const App = () => {
   return (
     <>
       <Container className="main">
+       
+          <Navbar />
 
-        <Navbar/>
-        
-        <Home/>
-        <Features/>
-        <Services/>
-        <Pricing/>
-        <ContactUs/>
+          <Home />
+          <Features />
+          <Services />
+          <Pricing />
+          <ContactUs />
 
       </Container>
     </>
---
+++ src/components/ExploreCourses.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import CourseCard from './CourseCard';
+const ExploreCourses = () => {
+  const courses = [
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Web Development Bootcamp",
+      duration: "6 Weeks",
+      amount: 299,
+      description:
+        "Learn the fundamentals of web development with this comprehensive course.",
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Data Science Essentials",
+      duration: "8 Weeks",
+      amount: 399,
+      description:
+        "Dive into data science and learn how to analyze data effectively.",
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Digital Marketing Masterclass",
+      duration: "4 Weeks",
+      amount: 199,
+      description:
+        "Master digital marketing strategies and grow your business online.",
+    },
+  ];
+
+  return (
+    <div className="explore-courses">
+      <h1>Explore Our Courses</h1>
+      <div className="course-list">
+        {courses.map((course, index) => (
+          <CourseCard
+            key={index}
+            image={course.image}
+            title={course.title}
+            duration={course.duration}
+            amount={course.amount}
+            description={course.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExploreCourses;
