ca43b1c56262dd67b11021565c962161e9f9aa1e
Jabarsonrichardgs <jabarsonrichardofficial@gmail.com>
2024-10-25 20:42:28 +0530

    Your commit message

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 import React from "react";
 
 const Animations = () => {
@@ -14,20 +15,4 @@ const Animations = () => {
 };
 
 export default Animations;
-=======
-import React from "react";
 
-const Animations = () => {
-  return (
-    <div>
-      <div className="circle"></div>
-      <div className="small-circle"></div>
-      <div className="small-circle-1"></div>
-
-
-    </div>
-  );
-};
-
-export default Animations;
->>>>>>> 5a84f5fdfd4b446c55c304790ce134a4a1595fe8
