de630b85881defede9e805d7c8b907da9b211f39
Jabarsonrichardgs <97936424+Jabarsonrichardgs@users.noreply.github.com>
2024-10-25 20:52:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,14 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 5a84f5fdfd4b446c55c304790ce134a4a1595fe8
